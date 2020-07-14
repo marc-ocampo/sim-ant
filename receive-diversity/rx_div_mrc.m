235256b52d5ddb7d394d2ff1aabd9d2cf35bf283
Marc Ocampo <mdocampo1@gmail.com>
2020-07-14 09:45:12 +0800

    split maximum ratio combining scheme to vectorized and non-vectorized function

--- receive-diversity/rx_div_mrc.m
+++ receive-diversity/rx_div_mrc.m
@@ -15,11 +15,19 @@ function y_div = rx_div_mrc(y, H)
   N = size(y)(2);
   y_div = zeros(1, N);
 
-%  for symb_idx = 1 : N % vectorize
-%    h = H(:, symb_idx);
-%    y_div(1, symb_idx) = y(:, symb_idx)' * h / norm(h);
-%  endfor
+  y_div = efficient_mrc(H, y);
+endfunction
 
+% efficient version of inefficient_mrc by utilizing vectorization
+function y_div = efficient_mrc(H, y)
   norm_H = norm(H, opt='columns'); % to get norm per symbol on different ants
   y_div = sum(y .* conj(H) ./ norm_H);
+endfunction
+
+% used originally due to its intuitiveness but is inefficient due to the loops
+function y_div = inefficient_mrc(N, H, y)
+  for symb_idx = 1 : N % vectorize
+    h = H(:, symb_idx);
+    y_div(1, symb_idx) = y(:, symb_idx)' * h / norm(h);
+  endfor
 endfunction
