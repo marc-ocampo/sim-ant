2c2b671fe17550edc90a47bdccdb9a909d2abb7a
Marc Ocampo <mdocampo1@gmail.com>
2020-10-14 17:45:07 +0800

    new plot where beam is shifted to other directions

--- uniform-linear-ant-array/num_ae_vs_beam_pattern.m
+++ uniform-linear-ant-array/num_ae_vs_beam_pattern.m
@@ -8,6 +8,7 @@
 # 2. Beamwidth becomes sharper as the number of antenna elements increase.
 #    This goes for both its main lobe and its sidelobes.
 # 3. Sidelobes have increasing beamwidth as it moves farther from the main lobe.
+# 4. Sidelobes decreases amplitude as it moves farther from the main lobe.
 ################################################################################
 
 # Parameters
@@ -29,5 +30,29 @@ figure;
 plot(theta, AF_dB);
 title('Number of Antenna Elements and Beam Pattern')
 legend('N=8', 'N=16', 'N=32')
+xlabel('Beam angle (degrees)')
+ylabel('Amplitude (dB)')
+
+#### Another case where beam angle is shifted
+
+# Parameters
+N = 32;
+d_lambda = 0.5;
+beam_angle = [0 30 60]';
+theta = -90:1/100:90;
+
+# Calculation
+theta_rad = theta * pi/180;
+beam_angle_rad = beam_angle * pi/180;
+
+AF_num = sin(N * pi * d_lambda * (sin(theta_rad) - sin(beam_angle_rad)));
+AF_denum = N * sin(pi * d_lambda * (sin(theta_rad) - sin(beam_angle_rad)));
+AF_dB = 20*log10(AF_num./AF_denum);
+
+# Plot
+figure;
+plot(theta, AF_dB);
+title('Beam Angle and Antenna Factor')
+legend('0 deg', '30 deg', '60 deg')
 xlabel('Beam angle (degrees)')
 ylabel('Amplitude (dB)')
